5c88981add38aa9d23c0bae66e99a74b76fe3c1d
Manifoldz <56.zabolotnyi@gmail.com>
2024-10-28 20:17:47 +0500

    need refact

--- Lesson_1/task3/task3.c
+++ Lesson_1/task3/task3.c
@@ -107,56 +107,136 @@ int CountWidth(Input_t* input, int i, int j) {
   return counter;
 }
 
+void Swap(int* a, int* b) {
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+int Min(int a, int b) { return a < b ? a : b; }
+
 Output_t* Process(Input_t* input, Error_t* err) {
   if (*err != kOk) {
     return NULL;
   }
   Output_t* output = (Output_t*)SafeMalloc(sizeof(Output_t), err);
-  if (*err == kOk) {
-    int count_rect = 0;
-    output->answer = 'X';
-    Rect_t light_rect = {0, 0, 1, 1};
-    // Search light diods
-    for (int i = 1; i < input->n - 1; i++) {
-      for (int j = 1; j < input->n - 1; j++) {
-        if (input->arr[i][j] == '#') {
-          if (light_rect.x == 0) {
-            light_rect.y = i;
-            light_rect.x = j;
-          } else if (light_rect.x + light_rect.width <= j) {
-            ++light_rect.width;
-          } else if (light_rect.y + light_rect.height <= i) {
-            ++light_rect.height;
-          }
-        }
-      }
-    }
-    printf("\n%d %d\n", light_rect.y, light_rect.x);
-    printf("\n%d %d\n", light_rect.width, light_rect.height);
-    Rect_t rect[2] = {{0}, {0}};
-    // Mark rectangles
-    for (int i = light_rect.y; i < light_rect.y + light_rect.height; i++) {
-      for (int j = light_rect.x; j < light_rect.x + light_rect.width; j++) {
-        if (input->arr[i][j] == '.') {
-          if (count_rect == 2) {
-            return output;
-          }
-          rect[count_rect].y = i;
-          rect[count_rect].x = j;
-          rect[count_rect].width = CountWidth(input, i, j);
-          MarkWidth(input, i, j, rect[count_rect].width, count_rect);
-          while (CountWidth(input, i + rect[count_rect].height + 1, j) >=
-                     rect[count_rect].width &&
-                 rect[count_rect].y + rect[count_rect].height + 1 <
-                     light_rect.y + light_rect.height) {
-            ++rect[count_rect].height;
-            MarkWidth(input, i + rect[count_rect].height, j,
-                      rect[count_rect].width, count_rect);
-          }
-          ++count_rect;
+  if (*err != kOk) {
+    return NULL;
+  }
+  output->answer = 'X';
+  // Search light diods
+  Rect_t light_rect = {0, 0, 1, 1};
+  for (int i = 1; i < input->n - 1; i++) {
+    for (int j = 1; j < input->n - 1; j++) {
+      if (input->arr[i][j] == '#') {
+        if (light_rect.x == 0) {
+          light_rect.y = i;
+          light_rect.x = j;
+        } else if (light_rect.x + light_rect.width <= j) {
+          ++light_rect.width;
+        } else if (light_rect.y + light_rect.height <= i) {
+          ++light_rect.height;
         }
       }
     }
   }
+  if (light_rect.x == 0) {
+    return output;
+  }
+  // Mark rectangles
+  Rect_t rect[2] = {{0}, {0}};
+  int count_rect = 0;
+  for (int i = light_rect.y; i < light_rect.y + light_rect.height; i++) {
+    for (int j = light_rect.x; j < light_rect.x + light_rect.width; j++) {
+      if (input->arr[i][j] == '.') {
+        if (count_rect == 2) {
+          return output;
+        }
+        rect[count_rect].y = i;
+        rect[count_rect].x = j;
+        rect[count_rect].width = CountWidth(input, i, j);
+        const int border_x = light_rect.x + light_rect.width - 1;
+        if (rect[count_rect].width + rect[count_rect].x > border_x) {
+          rect[count_rect].width = border_x - rect[count_rect].x + 1;
+        }
+        MarkWidth(input, i, j, rect[count_rect].width, count_rect);
+        while (CountWidth(input, i + rect[count_rect].height + 1, j) >=
+                   rect[count_rect].width &&
+               rect[count_rect].y + rect[count_rect].height + 1 <
+                   light_rect.y + light_rect.height) {
+          ++rect[count_rect].height;
+          MarkWidth(input, i + rect[count_rect].height, j,
+                    rect[count_rect].width, count_rect);
+        }
+        ++rect[count_rect].height;
+        ++count_rect;
+      }
+    }
+  }
+  // Analys
+  if (count_rect == 0) {
+    output->answer = 'I';
+    return output;
+  }
+
+  const int x1 = light_rect.x;
+  const int y1 = light_rect.y + light_rect.height - 1;
+  const int x2 = light_rect.x + light_rect.width - 1;
+  const int y2 = light_rect.y;
+
+  int x3 = rect[0].x;
+  int y3 = rect[0].y + rect[0].height - 1;
+  int x4 = rect[0].x + rect[0].width - 1;
+  int y4 = rect[0].y;
+
+  // printf("%d", count_rect);
+
+  if (count_rect == 1) {
+    if (y1 > y3 && y3 > y4 && y4 > y2) {
+      // printf("yes");
+      // printf("%d %d %d %d", x1, x2, x3, x4);
+      if (x1 < x3 && x3 < x4) {
+        if (x4 < x2) {
+          output->answer = 'O';
+        } else if (x4 == x2) {
+          output->answer = 'C';
+        }
+      }
+    } else if (y1 > y3 && y3 > y4 && y4 == y2) {
+      if (x1 < x3 && x3 < x4 && x4 < x2 && x4 == x2) {
+        output->answer = 'L';
+      }
+    }
+    return output;
+  }
+
+  if (count_rect == 2) {
+    int x5 = rect[1].x;
+    int y5 = rect[1].y + rect[1].height - 1;
+    int x6 = rect[1].x + rect[1].width - 1;
+    int y6 = rect[1].y;
+    if (y6 > y4) {
+      Swap(&x5, &x3);
+      Swap(&y5, &y3);
+      Swap(&x6, &x4);
+      Swap(&y6, &y4);
+    }
+    if (x1 < x3 && x3 == x5 && x5 < x4 && x4 == x6 && x6 < x2) {
+      // printf("yes");
+      // printf("%d %d %d %d %d %d", y1, y2, y3, y4, y5, y6);
+      if (y1 == y3 && y3 > y4 && y4 > y5 && y5 > y6 && y6 == y2) {
+        // printf("yes");
+        output->answer = 'H';
+      }
+    } else if (x1 < x3 && x3 == x5 && x5 < x6 && x6 < x4 && x4 == x2) {
+      printf("yes");
+      printf("%d %d %d %d %d %d", y1, y2, y3, y4, y5, y6);
+      if (y1 == y3 && y3 > y4 && y4 > y5 && y5 > y6 && y6 > y2) {
+        printf("yes");
+        output->answer = 'P';
+      }
+    }
+  }
+
   return output;
 }
