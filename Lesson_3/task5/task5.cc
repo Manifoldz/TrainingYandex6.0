fa710c3ea1217338d8b3f29e5649f131e12cbd6f
Manifoldz <56.zabolotnyi@gmail.com>
2024-11-10 23:54:14 +0300

    first version

--- Lesson_3/task5/task5.cc
+++ Lesson_3/task5/task5.cc
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <stack>
+#include <stdexcept>
+#include <string>
 
 bool IsDigit(char c) { return c >= '0' && c <= '9'; }
 
-#if 0
-int Calculate(std::string &str) {
+#if 1
+int Calculate(std::string& str) {
+  str.push_back(' ');
   std::stack<int> s;
   int prev_top = 0;
   char prev_char = '0';
@@ -13,31 +16,40 @@ int Calculate(std::string &str) {
   for (char curr_char : str) {
     if (IsDigit(curr_char)) {
       if (IsDigit(prev_char)) {
-        temp_int = temp_int * 10 + (prev_char - '0');
+        temp_int = temp_int * 10 + (curr_char - '0');
       } else {
-        temp_int = prev_char - '0';
+        temp_int = curr_char - '0';
       }
-      prev_char = curr_char;
     } else {
       // push accumulated digits
       if (IsDigit(prev_char)) {
         s.push(temp_int);
       }
-      prev_top = s.top();
-      s.pop();
-      if (curr_char == '+') {
-        s.top() += prev_top;
-      } else if (curr_char == '*') {
-        s.top() *= prev_top;
-      } else if (curr_char == '-') {
-        if (IsDigit(prev_char)) {
+      if (curr_char != ' ') {
+        prev_top = s.top();
+        s.pop();
+        if (s.empty()) {
+          throw std::invalid_argument("don't have a second element in stack");
+        }
+        if (curr_char == '+') {
+          s.top() += prev_top;
+        } else if (curr_char == '*') {
+          s.top() *= prev_top;
+        } else if (curr_char == '-') {
           s.top() -= prev_top;
-        } else
+        } else {
+          throw std::invalid_argument("wrong symbol in calculate");
+        }
       }
     }
+    prev_char = curr_char;
   }
 
-  return temp_int;
+  if (s.size() != 1 || str.size() == 1) {
+    throw std::invalid_argument("not enough operators");
+  }
+
+  return s.top();
 }
 #endif
 
@@ -57,9 +69,10 @@ void AddSpace(std::string& str) {
   }
 }
 
-void AddOperator(std::string& str, char c) {
+void AddOperator(std::string& str, std::stack<char>& s) {
   AddSpace(str);
-  str += c;
+  str += s.top();
+  s.pop();
 }
 
 std::string Parse() {
@@ -76,8 +89,7 @@ std::string Parse() {
       s.push(curr_char);
     } else if (curr_char == ')') {
       while (!s.empty() && s.top() != '(') {
-        AddOperator(parsed_str, s.top());
-        s.pop();
+        AddOperator(parsed_str, s);
       }
       if (s.empty()) {
         throw std::invalid_argument("no finded '('");
@@ -85,16 +97,15 @@ std::string Parse() {
         s.pop();
       }
     } else if (curr_char == '+' || curr_char == '*' || curr_char == '-') {
+      AddSpace(parsed_str);
       // hack to fix unar minus
       if (curr_char == '-' && !IsDigit(prev_char)) {
-        AddSpace(parsed_str);
         parsed_str += "0 ";
       } else if (!IsDigit(prev_char)) {
         throw std::invalid_argument("several operators one by one");
       }
       while (!s.empty() && Priority(curr_char) <= Priority(s.top())) {
-        AddOperator(parsed_str, s.top());
-        s.pop();
+        AddOperator(parsed_str, s);
       }
       s.push(curr_char);
     } else {
@@ -108,23 +119,19 @@ std::string Parse() {
   }
 
   while (!s.empty()) {
-    AddOperator(parsed_str, s.top());
-    s.pop();
+    AddOperator(parsed_str, s);
   }
 
   return parsed_str;
 }
 
 int main(void) {
-  std::string parsed_str;
   try {
-    parsed_str = Parse();
+    std::string parsed_str = Parse();
+    // std::cout << parsed_str;
+    std::cout << Calculate(parsed_str);
   } catch (std::invalid_argument) {
     std::cout << "WRONG";
-    return 0;
   }
-
-  std::cout << parsed_str;
-
   return 0;
 }
